537f30abaef2c16eac3b7829a2fa1a0311d743e1
Andres Tiapa <tiapa.andres@gmail.com>
2020-11-08 23:48:15 -0300

    Progress bar listo - Resta @Input

--- src/app/pages/progress.component.ts
+++ src/app/pages/progress.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progresoHtml: number = 50;
+
+  cambiaProgreso (valor : number) {
+    console.log("cambia progreso: " + valor);
+    this.progresoHtml = this.progresoHtml + valor;
+    this.progresoHtml < 0 ? this.progresoHtml = 0 : this.progresoHtml;
+    this.progresoHtml > 100 ? this.progresoHtml = 100 :this.progresoHtml;
+  }
+
+  get getProgreso () {
+    return `${this.progresoHtml}%`;
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
